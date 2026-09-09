079443ad0118c9bf8f0ea49587b67ef9133208df
anrem <anrem@ANDRES_PORTATIL>
2026-09-09 20:16:54 +0200

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Just_A_Black_Screen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
